e4ce92db8894e0c0431e9d1aa46d0ca6c1cdb9bd
madonuko <madonuko@outlook.com>
2024-07-02 15:05:42 +0800

    fix(log): correct program name

--- src/log.nim
+++ src/log.nim
@@ -1,6 +1,6 @@
 import std/logging
 
-var logger = newConsoleLogger(fmtStr="mkfstab: $levelname: ", useStderr=true)
+var logger = newConsoleLogger(fmtStr="dive: $levelname: ", useStderr=true)
 addHandler logger
 
 export debug, error, fatal, info, log, notice, warn, logging
